0b26fde70e42dee13e1410a55202954df1e8bb69
Ajene <ajenec@gmail.com>
2024-10-07 22:56:21 -0400

    finish string

---
+++ index.js
@@ -0,0 +1,14 @@
+//Practice with Strings+Arrays in functions. No numbers
+
+
+const greet = (name) => {
+    console.log(`${name}: Whats up buddy?`);
+};
+
+greet('Chuck');
+
+const feeling = (str) => {
+  if (str === 'sad'){
+    return `${name}: `
+  }
+}
